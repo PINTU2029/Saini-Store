25c7b28eeca3c563d2fde7489bfacfc150b5ee35
PINTU SAINI <pmsaini577@gmail.com>
2026-05-28 10:17:18 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({ 
     baseURL: process.env.REACT_APP_API_URL || "https://saini-store-production.up.railway.app",
+});
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
