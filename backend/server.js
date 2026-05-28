91d91714805b903e14b41e458f3cf5bb961ff548
PINTU SAINI <pmsaini577@gmail.com>
2026-05-28 10:37:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
   origin: ["https://saini-store.vercel.app", "http://localhost:3000"], 
@@ -14,8 +16,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json({ limit: '50mb' })); 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 // --- 2. Database Connection ---
 mongoose.connect(process.env.MONGO_URI)
