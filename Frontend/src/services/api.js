d9fd78d3121e645aa460dbf63553ae2d767b0ea6
PINTU SAINI <pmsaini577@gmail.com>
2026-06-06 11:38:09 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({
-    baseURL: 'https://saini-store-xzq1.onrender.com' 
+    baseURL: 'https://saini-store-xzq1.onrender.com/api' 
 });
 
 API.interceptors.request.use((req) => {
