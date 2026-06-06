4fbcf2034699a0655e5bb2bc44b022ccfd552fed
PINTU SAINI <pmsaini577@gmail.com>
2026-06-06 11:24:12 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const cors = require('cors');
 
-app.use(cors({
-    origin: 'https://saini-store.vercel.app', 
-    credentials: true
-}));
+const API = axios.create({
+    baseURL: 'https://saini-store-xzq1.onrender.com' 
+});
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
