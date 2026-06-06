46a2332b4688d7835ab87a72ed39a8b6addfff1c
PINTU SAINI <pmsaini577@gmail.com>
2026-06-06 11:26:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
+app.use(cors({
+    origin: 'https://saini-store.vercel.app',
+    credentials: true
+}));
 
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
